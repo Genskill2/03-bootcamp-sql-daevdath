3fdcf6b4e3b20e2e8cce64ea4cea11fcb7cb037f
daevdath <devadath1143varma@gmail.com>
2021-06-19 15:54:47 +0530

    corrected create file

--- create.sql
+++ create.sql
@@ -1,15 +1,15 @@
 create table publisher(
- id serial primary key,
+ id serial primary key autoincrement,
  name text NOT NULL,
  country text NOT NULL
  );
 create table books(
- id serial primary key,
+ id serial primary key autoincrement,
  title text NOT NULL,
  publisher integer references publisher(id)
  ); 
 create table subjects(
- id serial primary key,
+ id serial primary key autoincrement,
  name text NOT NULL
  );
 create table book_subjects(
