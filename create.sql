50a9012482ad3ab776d3d456f23df9666934f1b3
daevdath <devadath1143varma@gmail.com>
2021-06-19 23:42:47 +0530

    correction

--- create.sql
+++ create.sql
@@ -1,19 +1,19 @@
 create table publisher(
- id integer primary key,
+ id integer primary key autoincrement,
  name text NOT NULL,
  country text NOT NULL
  );
 create table books(
- id integer primary key,
+ id integer primary key autoincrement,
  title text NOT NULL,
- publisher text, foreign key(publisher)  references publisher(id)
+ publisher integer references publisher(id)
  ); 
 create table subjects(
- id integer primary key,
+ id integer primary key autoincrement,
  name text NOT NULL
  );
 create table books_subjects(
- book text,subject text,foreign key(book) references books(id),
- foreign key(subject) references subjects(id)
+ book integer references books(id),
+ subject integer references subjects(id)
  );
 
